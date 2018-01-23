e73498c3bbd1f5ac42f40b76400611e4e3963a45
Matthias <info@matthias-andrasch.de>
2018-01-23 13:06:44 +0100

    add heinrich heine universität düsseldorf mediathek

--- js/oer-squirrel-provider-list.js
+++ js/oer-squirrel-provider-list.js
@@ -1,180 +1,151 @@
 // 2DO: use name space, ignore this file (and images) in repo for forks
-  var oer_provider_list = [
-    {
-      'name':'SEGU Geschichte',
-      'url':'segu-geschichte.de',
-      'image':'segu_geschichte.jpg'
-    },
-    {
-      'name':'Serlo',
-      'url':'de.serlo.org',
-      'image':'serlo.png'
-    },
-    {
-      'name':'Memucho',
-      'url':'memucho.de',
-      'image':'memucho.png'
-    },
-    {
-      'name':'Medien in die Schule',
-      'url':'medien-in-die-schule.de',
-      'image':'medien-in-die-schule.jpg'
-    },
-    {
-      'name':'ZUM',
-      'url':'zum.de',
-      'image':'zum.jpg'
-    },
-    {
-      'name':'Wikiversity',
-      'url':'de.wikiversity.org',
-      'image':'wikiversity.png'
-    },
-    {
-      'name':'EDUdigitalLE',
-      'url':'oer.uni-leipzig.de',
-      'image':'edudigitalle.png'
-    },
-    {
-      'name':'rpi virtuell',
-      'url': 'material.rpi-virtuell.de',
-      'image':'rpi_virtuell.png'
-    },
-    {
-      'name':'Offene Naturführer',
-      'url':'offene-naturfuehrer.de',
-      'image':'offene_naturfuehrer.png'
-    },
-    {
-      'name':'KindOERgarten',
-      'url':'kindoergarten.de',
-      'image':'kindoergarten.png'
-    },
-    {
-      'name':'teachSam',
-      'url':'teachsam.de',
-      'image':'teachsam.png'
-    },
-    {
-      'name':'IDeRblog - Übungsdatenbank',
-      'url':'typo3.lpm.uni-sb.de/iderblog/fuer-erwachsene/uebungsdatenbank-online/',
-      'image':'iderblog.png'
-    },
-    {
-      'name':'ChiLe - Chemie interaktiv lernen',
-      'url':'chemieunterricht-interaktiv.de',
-      'image':'chile.png'
-    },
-    {
-      'name':'Ö1 macht Schule',
-      'url':'www.oe1macht.schule',
-      'image':'oe1machtschule.png'
-    }
-  ];
+var oer_provider_list = [{
+  'name': 'SEGU Geschichte',
+  'url': 'segu-geschichte.de',
+  'image': 'segu_geschichte.jpg'
+}, {
+  'name': 'Serlo',
+  'url': 'de.serlo.org',
+  'image': 'serlo.png'
+}, {
+  'name': 'Memucho',
+  'url': 'memucho.de',
+  'image': 'memucho.png'
+}, {
+  'name': 'Medien in die Schule',
+  'url': 'medien-in-die-schule.de',
+  'image': 'medien-in-die-schule.jpg'
+}, {
+  'name': 'ZUM',
+  'url': 'zum.de',
+  'image': 'zum.jpg'
+}, {
+  'name': 'Wikiversity',
+  'url': 'de.wikiversity.org',
+  'image': 'wikiversity.png'
+}, {
+  'name': 'EDUdigitalLE',
+  'url': 'oer.uni-leipzig.de',
+  'image': 'edudigitalle.png'
+}, {
+  'name': 'rpi virtuell',
+  'url': 'material.rpi-virtuell.de',
+  'image': 'rpi_virtuell.png'
+}, {
+  'name': 'Offene Naturführer',
+  'url': 'offene-naturfuehrer.de',
+  'image': 'offene_naturfuehrer.png'
+}, {
+  'name': 'KindOERgarten',
+  'url': 'kindoergarten.de',
+  'image': 'kindoergarten.png'
+}, {
+  'name': 'teachSam',
+  'url': 'teachsam.de',
+  'image': 'teachsam.png'
+}, {
+  'name': 'IDeRblog - Übungsdatenbank',
+  'url': 'typo3.lpm.uni-sb.de/iderblog/fuer-erwachsene/uebungsdatenbank-online/',
+  'image': 'iderblog.png'
+}, {
+  'name': 'ChiLe - Chemie interaktiv lernen',
+  'url': 'chemieunterricht-interaktiv.de',
+  'image': 'chile.png'
+}, {
+  'name': 'Ö1 macht Schule',
+  'url': 'www.oe1macht.schule',
+  'image': 'oe1machtschule.png'
+}];
 
-  var mixed_provider_list =
-  [
-    {
-      'name':'Bundeszentrale für Politische Bildung',
-      'url':'bpb.de',
-      'image':'bpb.jpg'
-    },
-    {
-      'name':'iMooX',
-      'url':'imoox.at',
-      'image':'imoox.png'
-    },    
-    {
-      'name':'Lecture2Go Universität Hamburg',
-      'url':'lecture2go.uni-hamburg.de',
-      'image':'lecture2go.png'
-    },
-    {
-      'name':'Lehrer Online',
-      'url':'lehrer-online.de',
-      'image':'lehrer_online.png'
-    },
-    {
-      'name':'Medienportal Siemens Stiftung',
-      'url':'medienportal.siemens-stiftung.org',
-      'image':'siemens_stiftung.png'
-    },
-    {
-      'name':'MOOIN',
-      'url':'mooin.oncampus.de',
-      'image':'mooin.png'
-    },
-	{
-      'name':'openlearnware - TU Darmstadt',
-      'url':'openlearnware.de',
-      'image':'openlearnware.png'
-    },
-    {
-		'name':'tutory.de - Arbeitsblätter online gestalten',
-		'url':'tutory.de',
-		'image':'tutory.png'
-    }
-  ];
+var mixed_provider_list =
+  [{
+    'name': 'Bundeszentrale für Politische Bildung',
+    'url': 'bpb.de',
+    'image': 'bpb.jpg'
+  }, {
+    'name': 'iMooX',
+    'url': 'imoox.at',
+    'image': 'imoox.png'
+  }, {
+    'name': 'Lecture2Go Universität Hamburg',
+    'url': 'lecture2go.uni-hamburg.de',
+    'image': 'lecture2go.png'
+  }, {
+    'name': 'Lehrer Online',
+    'url': 'lehrer-online.de',
+    'image': 'lehrer_online.png'
+  }, {
+    'name': 'Medienportal Siemens Stiftung',
+    'url': 'medienportal.siemens-stiftung.org',
+    'image': 'siemens_stiftung.png'
+  }, {
+    'name': 'MOOIN',
+    'url': 'mooin.oncampus.de',
+    'image': 'mooin.png'
+  }, {
+    'name': 'openlearnware - TU Darmstadt',
+    'url': 'openlearnware.de',
+    'image': 'openlearnware.png'
+  }, {
+    'name': 'tutory.de - Arbeitsblätter online gestalten',
+    'url': 'tutory.de',
+    'image': 'tutory.png'
+  }, {
+    'name':'Heinrich-Heine-Universität Düsseldorf Mediathek',
+    'url':'mediathek.hhu.de',
+    'image':'hhumediathek.png'
+  }];
 
 
-  var media_provider_list = [
-    {
-      'id':'youtube',
-      'name':'YouTube',
-      'url':'youtube.com',
-      'image':'youtube.png'
-    },
-    {
-      'id':'flickr',
-      'name':'flickr',
-      'url':'flickr.com',
-      'image':'flickr.png'
-    },
-    {
-      'id':'googleimages',
-      'name':'Google Bildersuche',
-      'url':'google.de/imghp',
-      'image':'google_image_search.png'
-    },
-    {
-      'id':'pixabay',
-      'name':'Pixabay (CC0 Fotos/Bilder)',
-      'url':'pixabay.de',
-      'image':'pixabay.png'
-    },
-    {
-      'id':'freemusicarchive',
-      'name':'Free Music Archive (FMA)',
-      'url':'freemusicarchive.com',
-      'image':'freemusicarchive.png'
-    },{
-      'id':'wikimediacommons',
-      'name':'Wikimedia Commons',
-      'url':'commons.wikimedia.org',
-      'image':'wikimedia_commons.png'
-    },
-    {
-      'id':'edutags',
-      'name':'Edutags - Social Bookmarking für Lehr- und Lernmaterial',
-      'url':'edutags.de',
-      'image':'edutags.png'
-    },
+var media_provider_list = [{
+    'id': 'youtube',
+    'name': 'YouTube',
+    'url': 'youtube.com',
+    'image': 'youtube.png'
+  }, {
+    'id': 'flickr',
+    'name': 'flickr',
+    'url': 'flickr.com',
+    'image': 'flickr.png'
+  }, {
+    'id': 'googleimages',
+    'name': 'Google Bildersuche',
+    'url': 'google.de/imghp',
+    'image': 'google_image_search.png'
+  }, {
+    'id': 'pixabay',
+    'name': 'Pixabay (CC0 Fotos/Bilder)',
+    'url': 'pixabay.de',
+    'image': 'pixabay.png'
+  }, {
+    'id': 'freemusicarchive',
+    'name': 'Free Music Archive (FMA)',
+    'url': 'freemusicarchive.com',
+    'image': 'freemusicarchive.png'
+  }, {
+    'id': 'wikimediacommons',
+    'name': 'Wikimedia Commons',
+    'url': 'commons.wikimedia.org',
+    'image': 'wikimedia_commons.png'
+  }, {
+    'id': 'edutags',
+    'name': 'Edutags - Social Bookmarking für Lehr- und Lernmaterial',
+    'url': 'edutags.de',
+    'image': 'edutags.png'
+  }
 
-  ]
+]
 
-  var not_compatible_provider_list =
+var not_compatible_provider_list =
   [
 
     {
-      'name':'Elixier Bildungssuchmaschine',
-      'url':'bildungsserver.de/elixier/',
-      'image':'elixier.png'
-    },
-    {
-      'name':'Learnline NRW',
-      'url':'learnline.schulministerium.nrw.de',
-      'image':'learnline.png'
+      'name': 'Elixier Bildungssuchmaschine',
+      'url': 'bildungsserver.de/elixier/',
+      'image': 'elixier.png'
+    }, {
+      'name': 'Learnline NRW',
+      'url': 'learnline.schulministerium.nrw.de',
+      'image': 'learnline.png'
     }
   ]
-
-
